79f9772fed7302c729f7078440812e1fd5c05ab2
niuhongtao <18333602833@163.com>
2019-07-09 17:46:09 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,55 @@
+const express=require('express');
+const path=require('path');
+const mongoose=require('mongoose');
+const app=express();
+const bodyParser=require('body-parser');
+const { check, validationResult } = require('express-validator/check');
+let Article=require('./models/article.js');
+const session=require('express-session');
+const passport=require('passport'); 
+const config=require('./config/database');
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','pug'); 
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname,'public')));
+require('./config/passport')(passport);
+app.use(session({
+    secret:config.secret,
+    resave: false,
+    saveUninitialized: true,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.get('*',function(req,res,next){
+    res.locals.user=req.user || null;
+    next();
+});
+mongoose.connect(config.database,{useNewUrlParser:true});
+let db=mongoose.connection;
+db.once('open',function(){
+    console.log('Connect to MongoDB');
+});
+db.on('error',function(err){
+    if(err) throw err;
+});
+app.get('/',(req,res)=>{
+    Article.find({},(err,articles)=>{
+        if(err) throw err;
+        res.render('articles/index',{articles:articles});
+    })
+});
+let articles=require('./routes/articles');
+let users=require('./routes/users');
+app.use('/articles',articles);
+app.use('/users',users);
+app.listen(5000,'0.0.0.0',()=>{
+    console.log('服务启动');
+});
+// 在同一个局域网下，例如“Wi-Fi”已连接至“WeWork”，其 IP 地址为 10.165.107.101。，如果设置‘0.0.0.0’或者不写，
+//在本地和手机端都可以连接http://10.165.107.101:5000/
+//否则只能在本地电脑连接，在手机上是无法连接http://10.165.107.101:5000/的
---
+++ routes/users.js
@@ -0,0 +1,88 @@
+let express=require('express');
+let router=express.Router();
+let User=require('../models/user');
+const { check, validationResult } = require('express-validator/check');
+let bcrypt=require('bcrypt');
+const passport=require('passport');
+router.get('/register',(req,res)=>{
+    res.render('users/register')
+});
+function uniqObjInArray(objarray){
+    let len = objarray.length;
+    let tempJson = {
+        
+    };
+    let res = [];
+    for(let i = 0;i < len;i++){
+        //取出每一个对象
+        tempJson[JSON.stringify(objarray[i])] = true;
+    }
+    console.log("tempJson is ",tempJson);
+    let keyItems= Object.keys(tempJson);
+    for(let j = 0;j < keyItems.length;j++){
+        res.push(JSON.parse(keyItems[j]));
+    }
+    return res;
+}
+router.post('/register',[
+    check('name').isLength({min:1}).withMessage('Name is required'),
+    check('email').isLength({min:1}).withMessage('Email is required'),
+    check('username').isLength({min:1}).withMessage('Username is required'),
+    check('password_confirmation').isLength({min:1}).withMessage('Password-Confirmation is required'),
+    check('email').isEmail().withMessage('Email is invalid'),
+    check('password','password is required'),
+    check('password').isLength({min:1}).withMessage('password is required'),
+    check('password','password is required').trim().custom((value,{req,loc,path})=>{
+            if(value!==req.body.password_confirmation){
+                throw new Error('Passwords do not match');
+            }else {
+                return value;
+            }
+        })
+],(req,res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        // return res.status(422).json({ errors: errors.array() });
+        console.log(errors.array());
+        let errorsList=errors.array();
+        errorsList=uniqObjInArray(errorsList);
+        console.dir(errorsList);
+        res.render('users/register',{errors:errorsList,title:'Add article'});
+    }else{
+        let users=new User(req.body);
+        // const saltRounds = 10;//说明https://www.jianshu.com/p/2b131bfc2f10
+        bcrypt.genSalt(10, function(err, salt) {
+            bcrypt.hash(users.password, salt, function(err, hash) {
+                // Store hash in your password DB.
+                if(err){
+                    console.error(err);
+                    return;
+                }
+                users.password=hash;
+                users.save((err,data)=>{
+                    if(err) throw err;
+                    req.flash("success", "Register and Login");
+                    res.redirect('/users/login');
+                })
+            });
+        });
+    }
+}); 
+router.get('/login',(req,res)=>{
+    res.render('users/login')
+});
+router.post('/login',(req,res,next)=>{
+    passport.authenticate('local', 
+        {   successRedirect: '/',
+            failureRedirect: '/users/login',
+            failureFlash: true ,
+            successFlash:'Welcome!'
+        }
+    )(req,res,next)
+});
+router.get('/logout', function(req, res){
+    req.logout();
+    req.flash('success','You have logged out!');
+    res.redirect('/users/login');
+});
+module.exports=router;
