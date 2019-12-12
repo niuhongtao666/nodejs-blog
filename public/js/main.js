9ded9e54aa077b828a2ed5c5f162b0c4fa93a877
niuhongtao <18333602833@163.com>
2019-12-12 17:21:11 +0800

    main.js修改

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 $(function(){
     // zymedia('video',{autoplay: true});
-    new zymedia('video', {"autoplay": "true","preload": "none",});
+    new zymedia('video', {"autoplay": "true","preload": "auto",});
     $('.delete-article').on('click',function(e){
         console.log($(this).data('id'));
         $.ajax({
